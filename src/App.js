67aea6fb3f590e041997c370aa6dfa51a9ebedb9
adnan <adnanali.gd@gmail.com>
2021-06-19 00:02:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => setMonsters(users));
+  // }, []);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const URL = `https://jsonplaceholder.typicode.com/users`;
+      const response = await fetch(URL);
+      const resJson = await response.json();
+      console.log(resJson, "asdad");
+      setMonsters(resJson);
+    };
+    fetchApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
     </div>
   );
-}
+};
 
 export default App;
